66be01ab7fbfeb547eb7e18fa019875a25107bc0
randbotani <botani.rand@gmail.com>
2016-12-21 09:59:22 +0100

    rand

--- src/CatalogueClient.java
+++ src/CatalogueClient.java
@@ -1,12 +1,40 @@
+import java.io.*;
+import java.net.Socket;
 
 public class CatalogueClient {
 
     private String host;
     private int port;
-
+    R
     public CatalogueClient(String host, int port) {
         this.host = host;
         this.port = port;
     }
 
+    public void getCentralCatalog(String request) {
+
+        try {
+            Socket socket = new Socket(host, port);
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+
+
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(outputStream);
+            writer.println(request);
+            writer.flush();
+            for (String centralCatalog = reader.readLine(); centralCatalog != null; centralCatalog = reader.readLine()) {
+                String[] temp = centralCatalog.trim().split("\\s+");
+                catalog.add(new Person(temp[0], temp[1], temp[2], temp[3]));
+            }
+            reader.close();
+            writer.close();
+
+        } catch (Exception e) {
+            System.out.println("we can't load centralCatalog because there is no connection with server");
+        }
+
+
+    }
 }
